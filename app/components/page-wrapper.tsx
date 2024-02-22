c37ccf1efa88636b0f4cdad0533b56c4ee8b9d42
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-02-21 23:17:44 -0500

    Fix layout to use overflow on body

--- app/components/page-wrapper.tsx
+++ app/components/page-wrapper.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export function PageWrapper({ children }: PropsWithChildren) {
-  return <div className="w-full px-5 pt-8 md:px-8">{children}</div>;
+  return <div className="px-5 pt-8 md:px-8 lg:ml-60 xl:ml-72">{children}</div>;
 }
