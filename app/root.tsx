e2eee4bba6cc760ff17587a4e29f19406f350518
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-02-21 23:44:24 -0500

    Add theme color

--- app/root.tsx
+++ app/root.tsx
@@ -52,6 +52,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#181a1b" />
         <Meta />
         <Links />
       </head>
