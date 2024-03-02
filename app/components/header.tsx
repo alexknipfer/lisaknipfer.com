3638fa2290b7ff3fce94d9cbb60e21555387bea8
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-02 11:57:28 -0500

    Remove backdrop blur

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -55,7 +55,7 @@ export function Header({ settings, pageSlugsAndTitles }: Props) {
   )?.title;
 
   return (
-    <header className="supports-backdrop-blur:bg-white/8- sticky inset-0 z-50 flex h-12 w-full items-center gap-2 border-b border-zinc-200 bg-white bg-white/80 px-2 backdrop-blur md:px-4 lg:hidden">
+    <header className="sticky inset-0 z-50 flex h-12 w-full items-center gap-2 border-b border-zinc-200 bg-white px-2 md:px-4 lg:hidden">
       <MobileDrawer settings={settings} />
       <span
         className="text-sm font-semibold"
