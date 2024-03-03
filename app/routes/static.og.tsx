7a56b5e54c1c2cf3a9531069f0e25d2a1da4ca87
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-02 23:39:46 -0500

    Testing  font

--- app/routes/static.og.tsx
+++ app/routes/static.og.tsx
@@ -1,4 +1,4 @@
-import { LoaderFunctionArgs } from '@remix-run/node';
+// import { LoaderFunctionArgs } from '@remix-run/node';
 import { ImageResponse } from '@vercel/og';
 // import { readFileSync } from 'fs';
 // import { fileURLToPath } from 'url';
@@ -7,10 +7,11 @@ import { OpenGraphImage } from '~/components/og-image';
 
 // const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
 
-export async function loader({ request }: LoaderFunctionArgs) {
-  const { protocol, host } = new URL(request.url);
+export async function loader() {
+  // const { protocol, host } = new URL(request.url);
+  // console.log('meta: ', import.meta.url);
   const fontData = await fetch(
-    `${protocol}//${host}/assets/fonts/GeistMedium.otf`,
+    new URL('./public/assets/fonts/GeistMedium.otf', import.meta.url),
   ).then((res) => res.arrayBuffer());
 
   return new ImageResponse(<OpenGraphImage />, {
