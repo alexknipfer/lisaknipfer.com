8853b9b7f0bbbdd11d11bdab29f9d6989c296122
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-02 23:48:36 -0500

    Testing fetch of assets

--- app/routes/static.og.tsx
+++ app/routes/static.og.tsx
@@ -1,4 +1,5 @@
 // import { LoaderFunctionArgs } from '@remix-run/node';
+import { LoaderFunctionArgs } from '@remix-run/node';
 import { ImageResponse } from '@vercel/og';
 // import { readFileSync } from 'fs';
 // import { fileURLToPath } from 'url';
@@ -7,12 +8,16 @@ import { OpenGraphImage } from '~/components/og-image';
 
 // const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
   // const { protocol, host } = new URL(request.url);
   // console.log('meta: ', import.meta.url);
-  const fontData = await fetch(
-    new URL('./public/assets/fonts/GeistMedium.otf', import.meta.url),
-  ).then((res) => res.arrayBuffer());
+  // const fontData = await fetch(
+  //   new URL('./public/assets/fonts/GeistMedium.otf', import.meta.url),
+  // ).then((res) => res.arrayBuffer());
+  const url = new URL(request.url);
+  const testUrl = new URL('/assets/fonts/GeistMedium.otf', url.origin);
+  console.log('test url: ', testUrl);
+  const fontData = await fetch(testUrl).then((res) => res.arrayBuffer());
 
   return new ImageResponse(<OpenGraphImage />, {
     width: 1200,
