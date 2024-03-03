8adfedf2b80ec3b225f1093796d03f3cef6131f1
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-03 00:02:01 -0500

    Cleanup

--- app/routes/static.og.tsx
+++ app/routes/static.og.tsx
@@ -1,35 +1,16 @@
-// import { LoaderFunctionArgs } from '@remix-run/node';
 import { LoaderFunctionArgs } from '@remix-run/node';
-// import { ImageResponse } from '@vercel/og';
 import satori from 'satori';
 import { Resvg } from '@resvg/resvg-js';
-// import { readFileSync } from 'fs';
-// import { fileURLToPath } from 'url';
 
-// import { OpenGraphImage } from '~/components/og-image';
-
-// const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
+import { OpenGraphImage } from '~/components/og-image';
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const testUrl = new URL('/assets/fonts/GeistMedium.otf', url.origin);
-  console.log('test url: ', testUrl);
-  const fontData = await fetch(testUrl).then((res) => res.arrayBuffer());
+  const requestUrl = new URL(request.url);
+  const fontUrl = new URL('/assets/fonts/GeistMedium.otf', requestUrl.origin);
+  const response = await fetch(fontUrl);
+  const fontData = await response.arrayBuffer();
 
-  // return new ImageResponse(<OpenGraphImage />, {
-  //   width: 1200,
-  //   height: 630,
-  //   fonts: [
-  //     {
-  //       name: 'Geist',
-  //       data: fontData,
-  //       style: 'normal',
-  //     },
-  //   ],
-  // });
-  const jsx = <div style={{ color: 'black' }}>hello, world</div>;
-  // From satori docs example
-  const svg = await satori(jsx, {
+  const svg = await satori(<OpenGraphImage />, {
     width: 1200,
     height: 630,
     fonts: [
@@ -40,10 +21,8 @@ export async function loader({ request }: LoaderFunctionArgs) {
       },
     ],
   });
-  const resvg = new Resvg(svg);
-  const pngData = resvg.render();
-  const data = pngData.asPng();
-  return new Response(data, {
+
+  return new Response(new Resvg(svg).render().asPng(), {
     headers: {
       'Content-Type': 'image/png',
     },
