0d0e3135f53aea4b8e3e76c1a4f6321b23a71ff3
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-02 23:57:08 -0500

    Testing og image

--- app/routes/static.og.tsx
+++ app/routes/static.og.tsx
@@ -1,25 +1,35 @@
 // import { LoaderFunctionArgs } from '@remix-run/node';
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { ImageResponse } from '@vercel/og';
+// import { ImageResponse } from '@vercel/og';
+import satori from 'satori';
+import { Resvg } from '@resvg/resvg-js';
 // import { readFileSync } from 'fs';
 // import { fileURLToPath } from 'url';
 
-import { OpenGraphImage } from '~/components/og-image';
+// import { OpenGraphImage } from '~/components/og-image';
 
 // const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  // const { protocol, host } = new URL(request.url);
-  // console.log('meta: ', import.meta.url);
-  // const fontData = await fetch(
-  //   new URL('./public/assets/fonts/GeistMedium.otf', import.meta.url),
-  // ).then((res) => res.arrayBuffer());
   const url = new URL(request.url);
   const testUrl = new URL('/assets/fonts/GeistMedium.otf', url.origin);
   console.log('test url: ', testUrl);
   const fontData = await fetch(testUrl).then((res) => res.arrayBuffer());
 
-  return new ImageResponse(<OpenGraphImage />, {
+  // return new ImageResponse(<OpenGraphImage />, {
+  //   width: 1200,
+  //   height: 630,
+  //   fonts: [
+  //     {
+  //       name: 'Geist',
+  //       data: fontData,
+  //       style: 'normal',
+  //     },
+  //   ],
+  // });
+  const jsx = <div style={{ color: 'black' }}>hello, world</div>;
+  // From satori docs example
+  const svg = await satori(jsx, {
     width: 1200,
     height: 630,
     fonts: [
@@ -29,5 +39,13 @@ export async function loader({ request }: LoaderFunctionArgs) {
         style: 'normal',
       },
     ],
+  });
+  const resvg = new Resvg(svg);
+  const pngData = resvg.render();
+  const data = pngData.asPng();
+  return new Response(data, {
+    headers: {
+      'Content-Type': 'image/png',
+    },
   });
 }
