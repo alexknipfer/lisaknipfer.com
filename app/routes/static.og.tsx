873ae646d892da7cb724c6026da68c4099b3a480
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-03-02 23:26:42 -0500

    Testing fetch from host

--- app/routes/static.og.tsx
+++ app/routes/static.og.tsx
@@ -1,18 +1,25 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
 import { ImageResponse } from '@vercel/og';
-import { readFileSync } from 'fs';
+// import { readFileSync } from 'fs';
+// import { fileURLToPath } from 'url';
 
 import { OpenGraphImage } from '~/components/og-image';
 
-const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
+// const getFont = () => readFileSync('./public/assets/fonts/GeistMedium.otf');
 
-export async function loader() {
+export async function loader({ request }: LoaderFunctionArgs) {
+  const { protocol, host } = new URL(request.url);
+  const fontData = await fetch(
+    `${protocol}//${host}/assets/fonts/GeistMedium.otf`,
+  ).then((res) => res.arrayBuffer());
+
   return new ImageResponse(<OpenGraphImage />, {
     width: 1200,
     height: 630,
     fonts: [
       {
         name: 'Geist',
-        data: getFont(),
+        data: fontData,
         style: 'normal',
       },
     ],
